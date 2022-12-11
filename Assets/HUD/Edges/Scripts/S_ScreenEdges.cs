1807e3d8450f1a60b7fd018bf3ca0c2ee22b3618
KevinGernigon <k.gernigon@rns.ecolescreatives.com>
2022-12-11 18:32:58 +0100

    HUD_Edges et modif input system (erreur u)

---
+++ Assets/HUD/Edges/Scripts/S_ScreenEdges.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_ScreenEdges : MonoBehaviour
+{
+    [SerializeField]
+    private List<Collider> _plateformesMouvantesList;
+
+    private Collider _storeCollider;
+
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private RawImage _screenEdges;
+
+    [SerializeField]
+    private List<Texture> _screenEdgesImages;
+
+    private void Start()
+    {
+        StartCoroutine(CheckPlayerDistanceToMovable());
+        StartCoroutine(AnimateScreenEdges());
+    }
+
+    IEnumerator AnimateScreenEdges()
+    {
+        while (true)
+        {
+            for(int i = 0; i < _screenEdgesImages.Count; i++)
+            {
+                _screenEdges.texture = _screenEdgesImages[i];
+                yield return new WaitForSeconds(0.15f);
+                if (i == 2)
+                {
+                    i = 0;
+                }
+            }
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+
+    IEnumerator CheckPlayerDistanceToMovable()
+    {
+        while (true)
+        {
+            _storeCollider = _plateformesMouvantesList[0];
+            for (int i = 1; i < _plateformesMouvantesList.Count; i++)
+            {
+                if (Vector3.Distance(_player.transform.position, _plateformesMouvantesList[i].transform.position) < Vector3.Distance(_player.transform.position, _storeCollider.transform.position))
+                {
+                    _storeCollider = _plateformesMouvantesList[i];
+                }
+
+            }
+            _screenEdges.color = new Color(_screenEdges.color.r, _screenEdges.color.b, _screenEdges.color.g, (2 / (1.5f * Vector3.Distance(_player.transform.position, _storeCollider.transform.position)) * 5));
+            if (_screenEdges.color.a < 0.05f)
+            {
+                _screenEdges.color = new Color(_screenEdges.color.r, _screenEdges.color.b, _screenEdges.color.g, 0.0f);
+            }
+            else if (_screenEdges.color.a >= 0.3f)
+            {
+                _screenEdges.color = new Color(_screenEdges.color.r, _screenEdges.color.b, _screenEdges.color.g, 0.5f);
+            }
+            Debug.Log(_screenEdges.color.a);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
