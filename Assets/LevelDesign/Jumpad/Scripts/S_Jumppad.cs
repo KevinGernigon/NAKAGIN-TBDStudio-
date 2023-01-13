e49602e9f59d6ec56a83694fc7901cfab793dd31
Tom Sachot <t.sachot@rns.ecolescreatives.com>
2023-01-13 10:34:46 +0100

    LD Hub 2 - Level 2 fin

--- Assets/LevelDesign/Jumpad/Scripts/S_Jumppad.cs
+++ Assets/LevelDesign/Jumpad/Scripts/S_Jumppad.cs
@@ -7,15 +7,16 @@ public class S_Jumppad : MonoBehaviour
     [SerializeField]
     Rigidbody _player;
     [SerializeField]
-    private float _addforce = 100f;
-
+    private float _addforceHauteur = 10f;
+    [SerializeField]
+    private float _addforceLongueur = 10f;
     void Start()
     {
         //_player = GetComponent<Rigidbody>();   
     }
     private void Jumpadbump()
     {
-        _player.velocity = new Vector3(_player.velocity.x * (_addforce/2), _addforce, _player.velocity.z);
+        _player.velocity = new Vector3(_player.velocity.x * (_addforceLongueur), _addforceHauteur, _player.velocity.z);
 
         _player.AddForce(transform.up, ForceMode.Impulse);
     }
